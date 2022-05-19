1267f2d6812b3921730a99e1a4604969d5948db3
phuongtrinh547 <truongtrinh547@gmail.com>
2022-05-19 10:11:13 +0700

    init work posting page

--- src/App.js
+++ src/App.js
@@ -1,8 +1,10 @@
 // import LoginPage from 'user/pages/Login/Login'
-import HomePage from 'user/pages/Home/Home'
+// import HomePage from 'user/pages/Home/Home'
+import WorkPostingPage from 'user/pages/WorkPosting/WorkPosting'
 function App() {
    return (
-      <HomePage />
+      // <HomePage />
+      <WorkPostingPage />
    )
 }
 
---
+++ src/user/pages/WorkPosting/WorkPosting.js
@@ -0,0 +1,6 @@
+import './WorkPosting.scss'
+export default function WorkPostingPage() {
+   return (
+      <h1>WorkPostingPage</h1>
+   )
+}
